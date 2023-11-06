29327037c92480137fce858e94cd8499800b9877
igorliotta <liottaigor@gmail.com>
2023-11-06 16:47:11 +0100

    Added: PageFooter

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -3,10 +3,66 @@
 </script>
 
 <template>
-  <div>
-    <h1>Ciao</h1>
-  </div>
+  <footer class="page-footer">
+   <div class="row nav-footer">
+   <div class="info-footer">
+    <h3>Boolando S.r.l</h3>
+    <ul class="list">
+      <li>Informazioni Legali</li>
+      <li>Informativa sulla privacy</li>
+      <li>Diritto di recesso</li>
+    </ul>
+   </div>
+   <div>
+    <h3 class="info-footer">Trovaci anche su</h3>
+    <ul class="list">
+      <li>
+        <img class="social-icon" src="/img/580b57fcd9996e24bc43c53e.png" alt="">
+      </li>
+      <li>
+        <img class="social-icon" src="/img/facebook-logo-1-2.png" alt="">
+      </li>
+      <li>
+        <img class="social-icon" src="/img/1658586823instagram-logo-transparent.png" alt="">
+      </li>
+      <li>
+        <img class="social-icon" src="/img/youtube-logo-transparent-free-png.png" alt="">
+      </li>
+      <li>
+        <img class="social-icon" src="/img/pinterest-logo-emblem-png-11.png" alt="">
+      </li>
+    </ul>
+   </div>
+   </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
+
+.page-footer {
+  background-color: black;
+  color: white;
+}
+
+.nav-footer {
+  padding: 20px;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+}
+.list {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  padding: 10px 0;
+}
+
+.social-icon {
+  width: 20px;
+  aspect-ratio: 1;
+}
+
+.info-footer {
+  font-size: 13px;
+}
 </style>
